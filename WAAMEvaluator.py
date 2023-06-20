bb3e0433fc76da74904fc6c14c3b63caccd59af6
johannespusicha <johannes.pusicha@piller.com>
2023-06-20 18:47:24 +0200

    add docstring to evalRadius

--- WAAMEvaluator.py
+++ WAAMEvaluator.py
@@ -151,6 +151,17 @@ def plotCurrentGeo():
 
 
 def __evalRadius__(index, cnts, N, r):
+    """Computes the distance from the point at index (current base point) to the center point other than index closest to a Heuver's sphere of radius r to the base point.
+
+    Args:
+        index (integer): index of current base point
+        cnts (np.ndarray): nx3 matrix of center points 
+        N (np.ndarray): nx3 matrix of normal vectors
+        r (float): radius
+
+    Returns:
+        float: distance
+    """
     basePnt = cnts[index]
     distances = np.linalg.norm((basePnt - cnts) * N, axis=1)
     distances[index] = np.inf
