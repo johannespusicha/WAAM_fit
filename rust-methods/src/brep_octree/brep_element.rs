185f8d9e54038015edd438e0a0ec7643cf87a9f9
johannespusicha <johannes.pusicha@piller.com>
2023-08-07 21:14:50 +0200

    add addition and multiplication to Vector

--- rust-methods/src/brep_octree/brep_element.rs
+++ rust-methods/src/brep_octree/brep_element.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Mul};
+
 #[derive(Clone, Copy, PartialEq, Debug)]
 struct Vector3D {
     i: f64,
@@ -18,10 +20,37 @@ impl Vector3D {
         (self.i * self.i + self.j * self.j + self.k * self.k).sqrt()
     }
 }
+
+impl Add for Vector3D {
+    type Output = Vector3D;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            i: self.i + rhs.i,
+            j: self.j + rhs.j,
+            k: self.k + rhs.k,
+        }
+    }
+}
+
+impl<T> Mul<T> for Vector3D
+where
+    T: Into<f64>,
+{
+    type Output = Vector3D;
+
+    fn mul(self, scalar: T) -> Vector3D {
+        let scalar_f64: f64 = scalar.into();
+        Vector3D {
+            i: self.i * scalar_f64,
+            j: self.j * scalar_f64,
+            k: self.k * scalar_f64,
+        }
+    }
+}
 #[cfg(test)]
 mod brep_element_tests {
     use super::*;
-
 
     #[test]
     fn test_create_vector_from_non_f64() {
@@ -33,6 +62,19 @@ mod brep_element_tests {
                 k: 3.0
             }
         )
+    }
+    #[test]
+    fn test_add_vector3d_to_vector3d() {
+        let v1 = Vector3D::new(1.0, 2.0, 3.0);
+        let v2 = Vector3D::new(2.0, 3.0, 4.0);
+
+        assert_eq!(v1 + v2, Vector3D::new(3.0, 5.0, 7.0))
+    }
+
+    #[test]
+    fn test_mul_vector3d_scalar() {
+        let v = Vector3D::new(1.0, 2.0, 3.0);
+        assert_eq!(v * 3, Vector3D::new(3.0, 6.0, 9.0))
     }
 
     #[test]
