238685fce7836520095bdec3c7f008ed10bff097
johannespusicha <johannes.pusicha@piller.com>
2023-08-17 23:45:38 +0200

    add rust_method playground/benchmark

--- test_rust.py
+++ test_rust.py
@@ -1,3 +1,7 @@
-import rust_methods
+import timeit
+import statistics as stats
 
-print(rust_methods.sum_as_string(1, 2))
+times = timeit.repeat('rust_methods.get_sphere_radii(nc, vc, indices)', setup='import rust_methods, numpy; nc = numpy.loadtxt("./output/cnts.csv", delimiter=", "); vc = numpy.loadtxt("./output/N.csv", delimiter=", "); indices = list(range(0,nc.shape[0]));', number=100, repeat=100)
+mean = stats.fmean(times) / 100
+stdev = stats.stdev(times) / 100
+print("On average it took {:.2e} seconds with a standard deviation of {:.2e} seconds.".format(mean, stdev))
